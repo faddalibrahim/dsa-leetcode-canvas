95901c1db685ecc57bb0b41a9fad3bc8562987ec
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-02-14 19:55:22 +0000

    chore: use only @ as reference for aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
