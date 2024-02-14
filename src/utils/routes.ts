0aa75311c76dfd6d5c0e9ff8f1f0153c779894f4
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-02-14 20:05:36 +0000

    feat: add page not found route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,4 +1,4 @@
-// student routes
 export const HOME_PAGE = "/";
 export const PATTERNS = `${HOME_PAGE}patterns`;
 export const SLIDING_WINDOW = `${PATTERNS}/sliding-window`;
+export const PAGE_NOT_FOUND = "*";
