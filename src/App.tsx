f55ddd931b4ee494e7bd7a3d5f9ffd6514f32734
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-02-14 20:44:15 +0000

    feat: route homepage, sliding_window and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// libraries
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// utils
+import { HOME_PAGE, SLIDING_WINDOW, PAGE_NOT_FOUND } from "@/utils/routes";
+import SlidingWindow from "@/pages/patterns/sliding_window/SlidingWindow";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path={HOME_PAGE} element={<>Home</>} />
+        <Route path={SLIDING_WINDOW} element={<SlidingWindow />} />
+        <Route path={PAGE_NOT_FOUND} element={<div>Page Not Found</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
