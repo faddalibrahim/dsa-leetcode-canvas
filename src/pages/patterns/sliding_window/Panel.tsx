511f432376616f9b127192f4cff315f3221069f2
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-02-16 10:46:20 +0000

    feat: create side panel for history, trace and code

---
+++ src/pages/patterns/sliding_window/Panel.tsx
@@ -0,0 +1,20 @@
+const Panel = () => {
+  return (
+    <div className="grid grid-rows-3 flow-col gap-1 w-[16vw]">
+      <div className="bg-[#222]">
+        <h1 className="text-orange-300">History</h1>
+        <div className="">hey</div>
+      </div>
+      <div className="bg-[#222]">
+        <h1 className="text-orange-300 h-full">Trace</h1>
+        <div className="">hell world</div>
+      </div>
+      <div className="bg-[#222]">
+        <h1 className="text-orange-300">Code</h1>
+        <div className="">hey</div>
+      </div>
+    </div>
+  );
+};
+
+export default Panel;
