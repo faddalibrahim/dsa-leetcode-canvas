1486c8103eede1a5b0dc06d7ccce0b8e0e3b2c43
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-02-16 10:47:55 +0000

    refactor: move functions to different file; fetch css variable values

--- src/pages/patterns/sliding_window/SlidingWindow.tsx
+++ src/pages/patterns/sliding_window/SlidingWindow.tsx
@@ -1,36 +1,49 @@
 // libraries
-import { useRef, useState } from "react";
-
-// components
-import AnimationControlBtns from "./AnimationControlBtns";
+import { useEffect, useRef, useState } from "react";
 
 // styles
 import "./sliding-window.css";
 
+// components
+import Panel from "./Panel";
+import ArrayInput from "./ArrayInput";
+import ControlButtons from "./ControlButtons";
+import * as ActionTypes from "./Controls";
+import * as ControlFunctions from "./ControlFunctions";
+
 export default function SlidingWindow() {
-  // refs and state
+  // refs
+  const containerRef = useRef<HTMLDivElement>(null);
   const frameRef = useRef<HTMLDivElement>(null);
   const slidingWindowRef = useRef<HTMLDivElement>(null);
-  const animationControlBtns = useRef<Map<
-    string | number,
-    HTMLButtonElement
-  > | null>(null);
+
   const cellRefs = useRef<Map<string | number, HTMLDivElement> | null>(null);
 
   // states
   const [cellValues, setCellValues] = useState<number[] | string[]>([
-    1, 2, 3, 4, 5,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
   ]);
 
-  function getButtonsMap() {
-    if (!animationControlBtns.current) {
-      animationControlBtns.current = new Map<
-        string | number,
-        HTMLButtonElement
-      >();
-    }
-    return animationControlBtns.current;
-  }
+  const [cellWidth, setCellWidth] = useState<number>(56);
+  const [gapBetweenCells, setGapBetweenCells] = useState<number>(0);
+
+  useEffect(() => {
+    const cellWidth = getComputedStyle(containerRef.current!).getPropertyValue(
+      "--cell-width"
+    );
+    const gapBetweenCells = getComputedStyle(
+      containerRef.current!
+    ).getPropertyValue("--gap-between-cells");
+    setCellWidth(parseInt(cellWidth));
+    setGapBetweenCells(parseInt(gapBetweenCells));
+    console.log(cellWidth);
+  }, []);
+
+  /**
+   * Get the map of cell references in the DOM
+   *
+   * @returns {Map<string | number, HTMLDivElement>} - The map of cells
+   */
 
   function getCellsMap() {
     if (!cellRefs.current) {
@@ -39,37 +52,85 @@ export default function SlidingWindow() {
     return cellRefs.current;
   }
 
-  function executeAction(buttonTitle: string) {
-    switch (buttonTitle) {
-      case "expand window":
-        expand();
+  /**
+   * Executes the specified action type
+   * and highlights the cells in the window view
+   *
+   * @param actionType - The type of action to execute
+   */
+
+  function executeAction(actionType: string) {
+    let startIndex: number, endIndex: number;
+
+    switch (actionType) {
+      case ActionTypes.EXPAND: {
+        if (willGoOutOfBounds()) return;
+        [startIndex, endIndex] = ControlFunctions.expand(
+          slidingWindowRef,
+          cellWidth,
+          gapBetweenCells
+        );
         break;
-      case "collapse window":
-        collapse();
+      }
+      case ActionTypes.COLLAPSE: {
+        if (willCollapseToZero()) return;
+        [startIndex, endIndex] = ControlFunctions.collapse(
+          slidingWindowRef,
+          cellWidth,
+          gapBetweenCells
+        );
         break;
-      case "slide window":
-        slide();
+      }
+      case ActionTypes.SLIDE: {
+        if (willGoOutOfBounds()) return;
+        [startIndex, endIndex] = ControlFunctions.slide(
+          slidingWindowRef,
+          cellWidth,
+          gapBetweenCells
+        );
         break;
-      case "reset":
-        reset();
+      }
+      case ActionTypes.RESET: {
+        [startIndex, endIndex] = ControlFunctions.reset(
+          slidingWindowRef,
+          cellWidth
+        );
         break;
+      }
       default:
         throw new Error("Invalid Action");
     }
+
+    highlightCellsInWindowView(startIndex, endIndex);
   }
+
+  function willCollapseToZero(): boolean {
+    return slidingWindowRef?.current?.clientWidth === cellWidth;
+  }
+
+  /**
+   * Checks if the sliding window will go out of bounds of the frame.
+   *
+   * @returns {boolean} - true if the window will go out of bounds, false otherwise
+   */
 
   function willGoOutOfBounds(): boolean {
     const windowSpace =
       (slidingWindowRef?.current?.offsetLeft ?? 0) +
-      (slidingWindowRef.current?.clientWidth ?? 0);
-    const frameWidth = frameRef?.current?.clientWidth;
+      (slidingWindowRef?.current?.clientWidth ?? 0);
+    const frameWidth = frameRef?.current?.clientWidth ?? 0;
 
-    return windowSpace === frameWidth;
+    console.log(windowSpace, frameWidth, windowSpace === frameWidth);
+
+    return windowSpace >= frameWidth;
   }
 
-  function willCollapseToZero(): boolean {
-    return slidingWindowRef?.current?.clientWidth === 80;
-  }
+  /**
+   * Highlights cells in the window view based on the start and end cell indices.
+   *
+   * @param startCell - The index of the starting cell
+   * @param endCell - The index of the ending cell
+   */
 
   function highlightCellsInWindowView(startCell: number, endCell: number) {
     for (let i = 0; i < cellValues.length; i++) {
@@ -84,108 +145,35 @@ export default function SlidingWindow() {
     }
   }
 
-  function expand() {
-    if (willGoOutOfBounds()) return;
-    if (slidingWindowRef?.current?.clientWidth) {
-      const newWindowWidth = slidingWindowRef?.current?.clientWidth + 80;
-      slidingWindowRef.current.style.width = `${newWindowWidth}px`;
-
-      const startIndex = slidingWindowRef.current.offsetLeft / 80;
-      const endIndex = startIndex + newWindowWidth / 80;
-
-      highlightCellsInWindowView(startIndex, endIndex);
-    }
-  }
-
-  function collapse() {
-    if (willCollapseToZero()) return;
-    if (slidingWindowRef?.current?.clientWidth) {
-      const newWindowOffsetLeft = slidingWindowRef.current.offsetLeft + 80;
-      slidingWindowRef.current.style.left = `${newWindowOffsetLeft}px`;
-
-      const newWindowWidth = slidingWindowRef.current.clientWidth - 80;
-      slidingWindowRef.current.style.width = `${newWindowWidth}px`;
-
-      const startIndex = newWindowOffsetLeft / 80;
-      const endIndex = startIndex + newWindowWidth / 80;
-
-      highlightCellsInWindowView(startIndex, endIndex);
-    }
-  }
-
-  function slide() {
-    if (willGoOutOfBounds()) return;
-
-    if (slidingWindowRef?.current?.clientWidth) {
-      const newWindowOffsetLeft = slidingWindowRef?.current?.offsetLeft + 80;
-      slidingWindowRef.current.style.left = `${newWindowOffsetLeft}px`;
-
-      const startIndex = newWindowOffsetLeft / 80;
-      const endIndex = startIndex + slidingWindowRef.current.clientWidth / 80;
-
-      highlightCellsInWindowView(startIndex, endIndex);
-    }
-  }
-
-  function reset() {
-    if (slidingWindowRef?.current?.clientWidth) {
-      slidingWindowRef.current.style.left = "0";
-      slidingWindowRef.current.style.width = "80px";
-
-      highlightCellsInWindowView(0, 1);
-    }
-  }
-
   return (
-    <div id="sliding-window-container">
-      <div id="history">history</div>
+    <div id="sliding-window-container" ref={containerRef}>
+      <Panel />
 
       <div id="canvas">
-        <div id="cell-values">
-          <input
-            type="text"
-            onChange={(e) => {
-              const newCellValue = e.target.value;
-              const newCellValueArray = [...newCellValue]
-                .map((cellValue) => cellValue.trim())
-                .filter((cellValue) => cellValue.length > 0);
-              setCellValues(newCellValueArray);
-            }}
-          />
-        </div>
-        <div id="frame" ref={frameRef}>
-          {cellValues.map((value, index) => (
-            <div
-              className={`cell ${index === 0 ? "highlight" : ""}`}
-              ref={(node) => {
-                const map = getCellsMap();
-                node
-                  ? map.set(index, node as HTMLDivElement)
-                  : map.delete(index);
-              }}
-            >
-              {value}
-            </div>
-          ))}
-          <div id="sliding-window" ref={slidingWindowRef}></div>
-        </div>
-        <div id="action-btns">
-          {AnimationControlBtns.map((btn) => (
-            <button
-              key={btn.title}
-              title={btn.title}
-              onClick={() => executeAction(btn.title)}
-              ref={(node) => {
-                const map = getButtonsMap();
-                node
-                  ? map.set(btn.title, node as HTMLButtonElement)
-                  : map.delete(btn.title);
-              }}
-            >
-              {btn.icon}
-            </button>
-          ))}
-        </div>
+        <ArrayInput setCellValues={setCellValues} />
+        {cellValues.length > 0 ? (
+          <div id="frame" ref={frameRef}>
+            {cellValues.map((value, index) => (
+              <div
+                key={`cell-${index}${value}`}
+                className={`cell ${index === 0 ? "highlight" : ""}`}
+                ref={(node) => {
+                  const map = getCellsMap();
+                  node
+                    ? map.set(index, node as HTMLDivElement)
+                    : map.delete(index);
+                }}
+              >
+                {value}
+              </div>
+            ))}
+            <div id="sliding-window" ref={slidingWindowRef}></div>
+          </div>
+        ) : (
+          <h1 className="text-[#aaa] text-xl">Enter some of values above</h1>
+        )}
+
+        <ControlButtons executeAction={executeAction} />
       </div>
     </div>
   );
